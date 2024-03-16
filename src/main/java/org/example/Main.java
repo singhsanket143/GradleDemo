2ff7b4ee991e550d18dd195f89886423603db4cc
Sanket Singh <singhsanket143@gmail.com>
2024-03-16 22:29:18 +0530

    Added okhttp to make http requests from plain java code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,33 @@
 package org.example;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Hello");
+        OkHttpClient client = new OkHttpClient();
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        String url = "https://example.com/";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if(!response.isSuccessful()) {
+                System.out.println("Something went wring !!!!");
+            }
+            System.out.println(response.body().string());
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
+
     }
 }
